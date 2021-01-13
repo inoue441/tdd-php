2ed69369aafad223ab74b40acfe56377945c9392
inoue441 <y.inoue@jfd.co.jp>
2021-01-14 08:10:03 +0900

    Red: Dollarオブジェクトを操作すると、Dollarオブジェクトの状態が変化してしまう

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -9,5 +9,8 @@ class MoneyTest extends TestCase
         $five = new Dollar(5);
         $five->times(2);
         $this->assertEquals(10, $five->amount);
+
+        $five->times(3);
+        $this->assertEquals(15, $five->amount);
     }
 }
