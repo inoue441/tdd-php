818b989517638661717bd5b54d03c37d99719ce8
inoue441 <y.inoue@jfd.co.jp>
2021-01-14 08:11:52 +0900

    Red: Failed asserting that null matches expected 10.

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -7,10 +7,10 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertEquals(10, $five->amount);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
 
-        $five->times(3);
-        $this->assertEquals(15, $five->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
     }
 }
