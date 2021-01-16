f84730b137575299f9c82109cedbd1a306e5c19a
inoue441 <y.inoue@jfd.co.jp>
2021-01-16 10:39:19 +0900

    Refactor: 最初のアサーションを修正して、Dollar同士を直接比較する

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -8,7 +8,7 @@ class MoneyTest extends TestCase
     {
         $five = new Dollar(5);
         $product = $five->times(2);
-        $this->assertEquals(10, $product->amount);
+        $this->assertEquals(new Dollar(10), $product);
 
         $product = $five->times(3);
         $this->assertEquals(15, $product->amount);
