8e192f5a8571095fe9585a24cd20b671e0b059c7
inoue441 <y.inoue@jfd.co.jp>
2021-01-18 09:32:47 +0900

    Red: 通貨

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -27,4 +27,9 @@ class MoneyTest extends TestCase
         $this->assertEquals(Money::franc(15), $five->times(3));
     }
 
+    public function testCurrency()
+    {
+        $this->assertEquals('USD', Money::dollar(1)->currency());
+        $this->assertEquals('CHF', Money::franc(1)->currency());
+    }
 }
