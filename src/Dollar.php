3e9d186cc8b8cfe12250932a6ee8fcab52077341
inoue441 <y.inoue@jfd.co.jp>
2021-01-18 21:14:51 +0900

    Green:

--- src/Dollar.php
+++ src/Dollar.php
@@ -13,6 +13,6 @@ class Dollar extends Money
      */
     public function times(int $multiplier)
     {
-        return new Dollar($this->amount * $multiplier, $this->currency);
+        return new Money($this->amount * $multiplier, $this->currency);
     }
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -7,8 +7,8 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = Money::dollar(5);
-        $this->assertEquals(Money::dollar(10), $five->times(2));
-        $this->assertEquals(Money::dollar(15), $five->times(3));
+        $this->assertTrue(Money::dollar(10)->equals($five->times(2)));
+        $this->assertTrue(Money::dollar(15)->equals($five->times(3)));
     }
 
     public function testEquality()
@@ -23,8 +23,8 @@ class MoneyTest extends TestCase
     public function testFrancMultiplication()
     {
         $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
+        $this->assertTrue(Money::franc(10)->equals($five->times(2)));
+        $this->assertTrue(Money::franc(15)->equals($five->times(3)));
     }
 
     public function testCurrency()
