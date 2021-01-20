3492929b7e2a95f43fb8e5888bbb48a3e4d59984
inoue441 <y.inoue@jfd.co.jp>
2021-01-21 08:24:14 +0900

    Red: 悪臭を放っているコード
    - キャストを行っている。本当はどんなExpressionであれ動作しなければならない
    - Sumのフィールドにアクセスしている。しかもそのフィールドのフィールドにさらにアクセスしている

--- src/Bank.php
+++ src/Bank.php
@@ -4,6 +4,9 @@ class Bank
 {
     public function reduce(Expression $source, string $to)
     {
-        return Money::dollar(10);
+        /** @var Sum $sum */
+        $sum = $source;
+        $amount = $sum->augend->amount + $sum->addend->amount;
+        return Money::dollar($amount);
     }
 }
